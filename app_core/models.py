d271880c0d5578b39a281718130cb8a84b0de8ba
Wilian Soares dos Santos <wilian.santos3@outlook.com>
2023-04-20 19:59:47 -0300

    Atualização BD 1.1

--- app_core/models.py
+++ app_core/models.py
@@ -1,6 +1,31 @@
 from django.db import models
 
 
-class Usuario(models.model):
+class Usuario(models.Model):
     nome = models.CharField('Nome', max_length=50)
+    email = models.EmailField()
+    senha = models.CharField(max_length=60)
+    renda = models.FloatField(null=True)
     
+    def __str__(self):
+        return self.nome
+    
+    
+class Rendas(models.Model):
+    renda_principal = models.FloatField()
+    renda_secundaria = models.FloatField()
+    data = models.DateTimeField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.renda_principal
+    
+    
+class Gastos(models.Model):
+    gasto = models.CharField('Gasto', max_length=80)
+    valor = models.FloatField()
+    data = models.DateTimeField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.gasto
