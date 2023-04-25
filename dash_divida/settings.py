7bcbcd85561ab98daab1d378c67c96f0adf34a23
Wilian Soares dos Santos <wilian.santos3@outlook.com>
2023-04-25 17:18:54 -0300

    Corrigindo erro

--- dash_divida/settings.py
+++ dash_divida/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from dash_divida.settings import DATABASES
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
